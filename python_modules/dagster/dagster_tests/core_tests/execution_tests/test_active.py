arpitjain799/dagster

thread instance through to active execution

python_modules/dagster/dagster_tests/core_tests/execution_tests/test_active.py
@@ -6,6 +6,7 @@ from dagster._core.execution.api import create_execution_plan
 from dagster._core.execution.plan.objects import StepSuccessData
 from dagster._core.execution.plan.outputs import StepOutputData, StepOutputHandle
 from dagster._core.execution.retries import RetryMode
+from dagster._core.test_utils import instance_for_test
 
 
 def define_foo_job():
@@ -23,48 +24,51 @@ def define_foo_job():
 def test_recover_with_step_in_flight():
     foo_job = define_foo_job()
 
-    with pytest.raises(
-        DagsterInvariantViolationError,
-        match="Execution finished without completing the execution plan",
-    ):
-        with create_execution_plan(foo_job).start(RetryMode.DISABLED) as active_execution:
-            steps = active_execution.get_steps_to_execute()
-            assert len(steps) == 1
-            step_1 = steps[0]
-            assert step_1.key == "foo_op"
+    with instance_for_test() as instance:
+        with pytest.raises(
+            DagsterInvariantViolationError,
+            match="Execution finished without completing the execution plan",
+        ):
+            with create_execution_plan(foo_job).start(
+                instance, RetryMode.DISABLED
+            ) as active_execution:
+                steps = active_execution.get_steps_to_execute()
+                assert len(steps) == 1
+                step_1 = steps[0]
+                assert step_1.key == "foo_op"
+
+                active_execution.handle_event(
+                    DagsterEvent(
+                        DagsterEventType.STEP_START.value,
+                        pipeline_name=foo_job.name,
+                        step_key=step_1.key,
+                    )
+                )
+
+        # CRASH!- we've closed the active execution. Now we recover, spinning up a new one
+
+        with create_execution_plan(foo_job).start(instance, RetryMode.DISABLED) as active_execution:
+            possibly_in_flight_steps = active_execution.rebuild_from_events(
+                [
+                    DagsterEvent(
+                        DagsterEventType.STEP_START.value,
+                        pipeline_name=foo_job.name,
+                        step_key=step_1.key,
+                    )
+                ]
+            )
+            assert possibly_in_flight_steps == [step_1]
+
+            assert not active_execution.get_steps_to_execute()
 
             active_execution.handle_event(
                 DagsterEvent(
-                    DagsterEventType.STEP_START.value,
+                    DagsterEventType.STEP_SUCCESS.value,
                     pipeline_name=foo_job.name,
+                    event_specific_data=StepSuccessData(duration_ms=10.0),
                     step_key=step_1.key,
                 )
             )
-
-    # CRASH!- we've closed the active execution. Now we recover, spinning up a new one
-
-    with create_execution_plan(foo_job).start(RetryMode.DISABLED) as active_execution:
-        possibly_in_flight_steps = active_execution.rebuild_from_events(
-            [
-                DagsterEvent(
-                    DagsterEventType.STEP_START.value,
-                    pipeline_name=foo_job.name,
-                    step_key=step_1.key,
-                )
-            ]
-        )
-        assert possibly_in_flight_steps == [step_1]
-
-        assert not active_execution.get_steps_to_execute()
-
-        active_execution.handle_event(
-            DagsterEvent(
-                DagsterEventType.STEP_SUCCESS.value,
-                pipeline_name=foo_job.name,
-                event_specific_data=StepSuccessData(duration_ms=10.0),
-                step_key=step_1.key,
-            )
-        )
 
 
 def define_two_op_job():
@@ -108,42 +112,47 @@ def test_recover_in_between_steps():
         ),
     ]
 
-    with pytest.raises(
-        DagsterInvariantViolationError,
-        match="Execution finished without completing the execution plan",
-    ):
-        with create_execution_plan(two_op_job).start(RetryMode.DISABLED) as active_execution:
-            steps = active_execution.get_steps_to_execute()
-            assert len(steps) == 1
-            step_1 = steps[0]
-            assert step_1.key == "foo_op"
+    with instance_for_test() as instance:
+        with pytest.raises(
+            DagsterInvariantViolationError,
+            match="Execution finished without completing the execution plan",
+        ):
+            with create_execution_plan(two_op_job).start(
+                instance, RetryMode.DISABLED
+            ) as active_execution:
+                steps = active_execution.get_steps_to_execute()
+                assert len(steps) == 1
+                step_1 = steps[0]
+                assert step_1.key == "foo_op"
 
-            active_execution.handle_event(events[0])
-            active_execution.handle_event(events[1])
-            active_execution.handle_event(events[2])
+                active_execution.handle_event(events[0])
+                active_execution.handle_event(events[1])
+                active_execution.handle_event(events[2])
 
-    # CRASH!- we've closed the active execution. Now we recover, spinning up a new one
+        # CRASH!- we've closed the active execution. Now we recover, spinning up a new one
 
-    with create_execution_plan(two_op_job).start(RetryMode.DISABLED) as active_execution:
-        possibly_in_flight_steps = active_execution.rebuild_from_events(events)
-        assert len(possibly_in_flight_steps) == 1
-        step_2 = possibly_in_flight_steps[0]
-        assert step_2.key == "bar_op"
+        with create_execution_plan(two_op_job).start(
+            instance, RetryMode.DISABLED
+        ) as active_execution:
+            possibly_in_flight_steps = active_execution.rebuild_from_events(events)
+            assert len(possibly_in_flight_steps) == 1
+            step_2 = possibly_in_flight_steps[0]
+            assert step_2.key == "bar_op"
 
-        assert not active_execution.get_steps_to_execute()
+            assert not active_execution.get_steps_to_execute()
 
-        active_execution.handle_event(
-            DagsterEvent(
-                DagsterEventType.STEP_START.value,
-                pipeline_name=two_op_job.name,
-                step_key="bar_op",
+            active_execution.handle_event(
+                DagsterEvent(
+                    DagsterEventType.STEP_START.value,
+                    pipeline_name=two_op_job.name,
+                    step_key="bar_op",
+                )
             )
-        )
-        active_execution.handle_event(
-            DagsterEvent(
-                DagsterEventType.STEP_SUCCESS.value,
-                pipeline_name=two_op_job.name,
-                event_specific_data=StepSuccessData(duration_ms=10.0),
-                step_key="bar_op",
+            active_execution.handle_event(
+                DagsterEvent(
+                    DagsterEventType.STEP_SUCCESS.value,
+                    pipeline_name=two_op_job.name,
+                    event_specific_data=StepSuccessData(duration_ms=10.0),
+                    step_key="bar_op",
+                )
             )
-        )

python_modules/libraries/dagster-celery/dagster_celery/core_execution_loop.py
@@ -56,6 +56,7 @@ def core_celery_execution_loop(pipeline_context, execution_plan, step_execution_
     step_errors = {}
 
     with execution_plan.start(
+        pipeline_context.instance,
         retry_mode=pipeline_context.executor.retries,
         sort_key_fn=priority_for_step,
     ) as active_execution:

ArnoudItility/dagster_spot_instances

python_modules/dagster/dagster_tests/core_tests/execution_tests/test_active.py
@@ -6,6 +6,7 @@ from dagster._core.execution.api import create_execution_plan
 from dagster._core.execution.plan.objects import StepSuccessData
 from dagster._core.execution.plan.outputs import StepOutputData, StepOutputHandle
 from dagster._core.execution.retries import RetryMode
+from dagster._core.test_utils import instance_for_test
 
 
 def define_foo_job():
@@ -23,48 +24,51 @@ def define_foo_job():
 def test_recover_with_step_in_flight():
     foo_job = define_foo_job()
 
-    with pytest.raises(
-        DagsterInvariantViolationError,
-        match="Execution finished without completing the execution plan",
-    ):
-        with create_execution_plan(foo_job).start(RetryMode.DISABLED) as active_execution:
-            steps = active_execution.get_steps_to_execute()
-            assert len(steps) == 1
-            step_1 = steps[0]
-            assert step_1.key == "foo_op"
+    with instance_for_test() as instance:
+        with pytest.raises(
+            DagsterInvariantViolationError,
+            match="Execution finished without completing the execution plan",
+        ):
+            with create_execution_plan(foo_job).start(
+                instance, RetryMode.DISABLED
+            ) as active_execution:
+                steps = active_execution.get_steps_to_execute()
+                assert len(steps) == 1
+                step_1 = steps[0]
+                assert step_1.key == "foo_op"
+
+                active_execution.handle_event(
+                    DagsterEvent(
+                        DagsterEventType.STEP_START.value,
+                        job_name=foo_job.name,
+                        step_key=step_1.key,
+                    )
+                )
+
+        # CRASH!- we've closed the active execution. Now we recover, spinning up a new one
+
+        with create_execution_plan(foo_job).start(instance, RetryMode.DISABLED) as active_execution:
+            possibly_in_flight_steps = active_execution.rebuild_from_events(
+                [
+                    DagsterEvent(
+                        DagsterEventType.STEP_START.value,
+                        job_name=foo_job.name,
+                        step_key=step_1.key,
+                    )
+                ]
+            )
+            assert possibly_in_flight_steps == [step_1]
+
+            assert not active_execution.get_steps_to_execute()
 
             active_execution.handle_event(
                 DagsterEvent(
-                    DagsterEventType.STEP_START.value,
+                    DagsterEventType.STEP_SUCCESS.value,
                     job_name=foo_job.name,
+                    event_specific_data=StepSuccessData(duration_ms=10.0),
                     step_key=step_1.key,
                 )
             )
-
-    # CRASH!- we've closed the active execution. Now we recover, spinning up a new one
-
-    with create_execution_plan(foo_job).start(RetryMode.DISABLED) as active_execution:
-        possibly_in_flight_steps = active_execution.rebuild_from_events(
-            [
-                DagsterEvent(
-                    DagsterEventType.STEP_START.value,
-                    job_name=foo_job.name,
-                    step_key=step_1.key,
-                )
-            ]
-        )
-        assert possibly_in_flight_steps == [step_1]
-
-        assert not active_execution.get_steps_to_execute()
-
-        active_execution.handle_event(
-            DagsterEvent(
-                DagsterEventType.STEP_SUCCESS.value,
-                job_name=foo_job.name,
-                event_specific_data=StepSuccessData(duration_ms=10.0),
-                step_key=step_1.key,
-            )
-        )
 
 
 def define_two_op_job():
@@ -108,42 +112,47 @@ def test_recover_in_between_steps():
         ),
     ]
 
-    with pytest.raises(
-        DagsterInvariantViolationError,
-        match="Execution finished without completing the execution plan",
-    ):
-        with create_execution_plan(two_op_job).start(RetryMode.DISABLED) as active_execution:
-            steps = active_execution.get_steps_to_execute()
-            assert len(steps) == 1
-            step_1 = steps[0]
-            assert step_1.key == "foo_op"
+    with instance_for_test() as instance:
+        with pytest.raises(
+            DagsterInvariantViolationError,
+            match="Execution finished without completing the execution plan",
+        ):
+            with create_execution_plan(two_op_job).start(
+                instance, RetryMode.DISABLED
+            ) as active_execution:
+                steps = active_execution.get_steps_to_execute()
+                assert len(steps) == 1
+                step_1 = steps[0]
+                assert step_1.key == "foo_op"
 
-            active_execution.handle_event(events[0])
-            active_execution.handle_event(events[1])
-            active_execution.handle_event(events[2])
+                active_execution.handle_event(events[0])
+                active_execution.handle_event(events[1])
+                active_execution.handle_event(events[2])
 
-    # CRASH!- we've closed the active execution. Now we recover, spinning up a new one
+        # CRASH!- we've closed the active execution. Now we recover, spinning up a new one
 
-    with create_execution_plan(two_op_job).start(RetryMode.DISABLED) as active_execution:
-        possibly_in_flight_steps = active_execution.rebuild_from_events(events)
-        assert len(possibly_in_flight_steps) == 1
-        step_2 = possibly_in_flight_steps[0]
-        assert step_2.key == "bar_op"
+        with create_execution_plan(two_op_job).start(
+            instance, RetryMode.DISABLED
+        ) as active_execution:
+            possibly_in_flight_steps = active_execution.rebuild_from_events(events)
+            assert len(possibly_in_flight_steps) == 1
+            step_2 = possibly_in_flight_steps[0]
+            assert step_2.key == "bar_op"
 
-        assert not active_execution.get_steps_to_execute()
+            assert not active_execution.get_steps_to_execute()
 
-        active_execution.handle_event(
-            DagsterEvent(
-                DagsterEventType.STEP_START.value,
-                job_name=two_op_job.name,
-                step_key="bar_op",
+            active_execution.handle_event(
+                DagsterEvent(
+                    DagsterEventType.STEP_START.value,
+                    job_name=two_op_job.name,
+                    step_key="bar_op",
+                )
             )
-        )
-        active_execution.handle_event(
-            DagsterEvent(
-                DagsterEventType.STEP_SUCCESS.value,
-                job_name=two_op_job.name,
-                event_specific_data=StepSuccessData(duration_ms=10.0),
-                step_key="bar_op",
+            active_execution.handle_event(
+                DagsterEvent(
+                    DagsterEventType.STEP_SUCCESS.value,
+                    job_name=two_op_job.name,
+                    event_specific_data=StepSuccessData(duration_ms=10.0),
+                    step_key="bar_op",
+                )
             )
-        )

python_modules/libraries/dagster-celery/dagster_celery/core_execution_loop.py
@@ -56,6 +56,7 @@ def core_celery_execution_loop(job_context, execution_plan, step_execution_fn):
     step_errors = {}
 
     with execution_plan.start(
+        job_context.instance,
         retry_mode=job_context.executor.retries,
         sort_key_fn=priority_for_step,
     ) as active_execution: